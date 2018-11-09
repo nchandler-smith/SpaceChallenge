5d02767857677727f614ed469c5ec0d4df726ee0
Nate Chandler-Smith <nchandler-smith@pillartechnology.com>
2018-11-08 20:38:12 -0600

    adding loadU1 in simulation class

--- src/main/Simulation.java
+++ src/main/Simulation.java
@@ -16,11 +16,29 @@ public class Simulation {
             while (scanner.hasNextLine()) {
                 String line = scanner.nextLine();
                 String[] parts = line.split("=");
-                Item item = new Item(parts[0], Integer.parseInt(parts[1]));
+                Item item = new Item(parts[0], Integer.parseInt(parts[1])/1000);
                 loadedItems.add(item);
             }
         } catch (FileNotFoundException e) {
         }
         return loadedItems;
+    }
+
+    public List<Rocket> loadU1(List<Item> loadedItems){
+        List<Rocket> rockets = new ArrayList();
+        U1 u1 = new U1();
+
+        for (Item item : loadedItems ) {
+            System.out.println("Rocket weight: " + u1.getWeight());
+            if (u1.canCarry(item)) {
+                u1.carry(item);
+            } else {
+                rockets.add(u1);
+                u1 = new U1();
+                u1.carry(item);
+            }
+        }
+        rockets.add(u1);
+        return rockets;
     }
 }
--- src/test/SimulationTest.java
+++ src/test/SimulationTest.java
@@ -9,18 +9,26 @@ class SimulationTest {
     @Test
     void ReadLineHabitatEquals100000Test() {
         Simulation sim = new Simulation();
-        String filename = "test.txt";
+        String filename = "SimulationTest.txt";
 
         assertEquals("habitat", sim.loadItems(filename).get(0).getName());
-        assertEquals(100000, sim.loadItems(filename).get(0).getWeight());
+        assertEquals(1, sim.loadItems(filename).get(0).getWeight());
     }
 
     @Test
     void ReadLineColonyEquals50000Test() {
         Simulation sim = new Simulation();
-        String filename = "test.txt";
+        String filename = "SimulationTest.txt";
 
         assertEquals("colony", sim.loadItems(filename).get(1).getName());
-        assertEquals(50000, sim.loadItems(filename).get(1).getWeight());
+        assertEquals(5, sim.loadItems(filename).get(1).getWeight());
+    }
+
+    @Test
+    void loadU1TestNeed2U1RocketsTest() {
+        Simulation sim = new Simulation();
+        String filename = "SimulationTest.txt";
+
+        assertEquals(2, sim.loadU1(sim.loadItems(filename)).size());
     }
 }
