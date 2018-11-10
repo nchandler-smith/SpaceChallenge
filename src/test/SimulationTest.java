f689061064530a145a54b837a9d189185c6897d3
Nate Chandler-Smith <nchandler-smith@pillartechnology.com>
2018-11-10 10:57:15 -0600

    refactored tests

--- src/test/SimulationTest.java
+++ src/test/SimulationTest.java
@@ -1,42 +1,41 @@
 package test;
 
 import main.Simulation;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class SimulationTest {
 
+    Simulation sim;
+    String simFile = "SimulationPayloadTest.txt";
+    String u1SimFile = "U1TestPayload.txt";
+    String u2SimFile = "U2TestPayload.txt";
+
+    @BeforeEach
+    void setup(){
+        sim = new Simulation();
+    }
+
     @Test
     void ReadLineHabitatEquals100000Test() {
-        Simulation sim = new Simulation();
-        String filename = "SimulationPayloadTest.txt";
-
-        assertEquals("habitat", sim.loadItems(filename).get(0).getName());
-        assertEquals(1, sim.loadItems(filename).get(0).getWeight());
+        assertEquals("habitat", sim.loadItems(simFile).get(0).getName());
+        assertEquals(1, sim.loadItems(simFile).get(0).getWeight());
     }
 
     @Test
     void ReadLineColonyEquals50000Test() {
-        Simulation sim = new Simulation();
-        String filename = "SimulationPayloadTest.txt";
-
-        assertEquals("colony", sim.loadItems(filename).get(1).getName());
-        assertEquals(5, sim.loadItems(filename).get(1).getWeight());
+        assertEquals("colony", sim.loadItems(simFile).get(1).getName());
+        assertEquals(5, sim.loadItems(simFile).get(1).getWeight());
     }
 
     @Test
     void loadU1Need2U1RocketsTest() {
-        Simulation sim = new Simulation();
-        String filename = "SimulationPayloadTest.txt";
-
-        assertEquals(2, sim.loadU1(sim.loadItems(filename)).size());
+        assertEquals(2, sim.loadU1(sim.loadItems(simFile)).size());
     }
 
     @Test
     void loadU2Need1U2RocketsTest() {
-        Simulation sim = new Simulation();
-        String filename = "SimulationPayloadTest.txt";
-
-        assertEquals(1, sim.loadU2(sim.loadItems(filename)).size());
+        assertEquals(1, sim.loadU2(sim.loadItems(simFile)).size());
     }
 }
