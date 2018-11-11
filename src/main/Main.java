2ea11948c19d76e16d3b8f2a4936fc935e2c759e
Nate Chandler-Smith <nchandler-smith@pillartechnology.com>
2018-11-10 19:31:13 -0600

    fixed a problem with instantiating new rockets in the simulation.

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,8 +1,19 @@
 package main;
 
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("Running phase 1 simulation with U1 rockets.");
+        System.out.println("-------------------------------------------");
+        Simulation sim = new Simulation();
+        List<Item> manifest = sim.loadItems("Phase1Payload.txt");
+        List<Rocket> fleet = sim.loadU1(manifest);
+        int budget = sim.runSimulation(fleet);
+        System.out.println("===================================");
+        System.out.println("This is expected to cost: " + budget);
 
+        
     }
 }
